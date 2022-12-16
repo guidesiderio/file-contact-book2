beb0b8c6979556055c5fc3ebcd15cd9520f29a68
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-15 22:37:20 -0300

    Create fuction insere_contato

--- app.py
+++ app.py
@@ -1,20 +1,29 @@
 # Inicializa a mini-agenda
-
-def inicializa_agenda(nome_arquivo = 'agenda.txt'):
+def inicializa_agenda(nome_arquivo):
     try:
-        f = open(nome_arquivo, 'w')
+        mini_agenda = open(nome_arquivo, 'w')
     except:
         print(f'Erro! Não foi póssivel iniciar o aquivo {nome_arquivo}')    
 
     return nome_arquivo
 
 # Ler um contato
-
 def ler_contato():
     id_contato = input('Id: ')
     nome_contato = input('Nome: ')
 
-    contato = (id_contato, nome_contato)
+    tupla_contato = (id_contato, nome_contato)
+
+    contato = contato[0] + ', ' + contato[1] + '\n'
 
     return contato
-        
+
+# Insere um novo contato
+def insere_contato(contato, nome_arquivo):    
+    mini_agenda = open(nome_arquivo, 'a')
+
+    novo_contato = ler_contato()
+    mini_agenda.write(novo_contato)
+
+
+    
