e95d85e57171985821c943617d87329a39735c5d
Guilherme Desidério <guilhermedesiderio23@gmail.com>
2022-12-15 22:30:19 -0300

    Create function ler_contato

--- app.py
+++ app.py
@@ -5,5 +5,16 @@ def inicializa_agenda(nome_arquivo = 'agenda.txt'):
         f = open(nome_arquivo, 'w')
     except:
         print(f'Erro! Não foi póssivel iniciar o aquivo {nome_arquivo}')    
-        
+
     return nome_arquivo
+
+# Ler um contato
+
+def ler_contato():
+    id_contato = input('Id: ')
+    nome_contato = input('Nome: ')
+
+    contato = (id_contato, nome_contato)
+
+    return contato
+        
